a0b408f590b9497c1e8169af4d0b7d79aedfe0f3
Nick Rossenbach <nick.rossenbach@rwth-aachen.de>
2023-05-30 13:50:43 +0200

    cleanup rnn.py

--- rnn.py
+++ rnn.py
@@ -9,11 +9,3 @@ from returnn.__main__ import main
 
 if __name__ == "__main__":
     main()
-
-else:
-    # This is likely an old script, directly importing this file.
-    # You should not do that. Import returnn.__main__ instead.
-    # Anyway, here some hacky workaround to keep this working.
-    import sys
-
-    sys.modules[__name__] = sys.modules["returnn.__main__"]
