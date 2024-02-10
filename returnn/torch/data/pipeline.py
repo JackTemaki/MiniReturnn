rwth-i6/returnn

PT convert np.uint16 to np.int32 (#1488)

returnn/torch/data/pipeline.py
@@ -42,6 +42,8 @@ def create_tensor(array: numpy.ndarray) -> Union[torch.Tensor, numpy.ndarray]:
         return array  # keep as-is. e.g. seq_tag
     if array.dtype == numpy.uint32:
         array = numpy.asarray(array, dtype=numpy.int64)
+    elif array.dtype == numpy.uint16:
+        array = numpy.asarray(array, dtype=numpy.int32)
     return torch.tensor(array)
 
 

JackTemaki/MiniReturnn

returnn/torch/data/pipeline.py
@@ -50,6 +50,8 @@ def create_tensor(value: InputType) -> OutputType:
         return value  # keep as-is
     if value.dtype == np.uint32:
         value = np.asarray(value, dtype=np.int64)
+    elif value.dtype == np.uint16:
+        value = np.asarray(value, dtype=np.int32)
     return torch.tensor(value)
 
 